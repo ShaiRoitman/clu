f429cda28a9cd6030f3572fd86bc441b57951aae
ShaiRoitman <Shai@Roitman.info>
2017-01-11 15:22:59 +0200

    Adding missing files

---
+++ clu/CommandLineUtility.cpp
@@ -0,0 +1,13 @@
+#include "Initialization.h"
+#include "CommandLineHandlers\CommandLineHandler.h"
+
+USING_NAMESPACE(clu)
+
+int main(int argc, const char* argv[])
+{
+	InitCluEnvironment();
+	CommandLineHandler& handler = CommandLineHandler::GetInstance();
+	handler.Initialize(argc, argv);
+	int ret_value = handler.ExecuteCommandLine();
+	return ret_value;
+}
