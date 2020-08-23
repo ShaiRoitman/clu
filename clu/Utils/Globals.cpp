6fc24e7b451a971511e18dc3f36e7b229e27e6f3
Shai Roitman <Shai@Roitman.info>
2020-08-23 08:10:06 +0300

    Bumped minor version

--- clu/Utils/Globals.cpp
+++ clu/Utils/Globals.cpp
@@ -1,5 +1,5 @@
 #include "Globals.h"
 
-const char* g_version = "5.5";
+const char* g_version = "5.5.1";
 const char* g_app_name = "Command Line Utility";
 const char* g_developer_name = "Shai Roitman";
